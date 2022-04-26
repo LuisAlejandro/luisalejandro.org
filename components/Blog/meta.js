8f51ccf78164d32352f11f29a0a45898711ba321
Luis Alejandro Martínez Faneyth <luis@luisalejandro.org>
2022-04-26 08:05:38 -0400

    [REF] Updating favicons.

--- components/Blog/meta.js
+++ components/Blog/meta.js
@@ -4,40 +4,23 @@ import { CMS_NAME, HOME_OG_IMAGE_URL } from '@/lib/constants'
 export default function Meta() {
   return (
     <Head>
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/favicon/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon/favicon-16x16.png"
-      />
+      <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg" />
+      <link rel="icon" type="image/png" href="/favicon/favicon.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
       <link rel="manifest" href="/favicon/site.webmanifest" />
-      <link
-        rel="mask-icon"
-        href="/favicon/safari-pinned-tab.svg"
-        color="#000000"
-      />
+      <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000" />
       <link rel="shortcut icon" href="/favicon/favicon.ico" />
-      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="msapplication-TileColor" content="#f8d983" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-      <meta name="theme-color" content="#000" />
+      <meta name="theme-color" content="#f8d983" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <link id="preload-sound-sprite" rel="preload" as="audio" type="audio/ogg" href="/sounds/sprite.ogg"></link>
+      <link id="preload-sound-sprite" rel="preload" as="audio" type="audio/ogg" href="/sounds/sprite.ogg" />
       <meta
         name="description"
-        content={`A statically generated blog example using Next.js and ${CMS_NAME}.`}
+        content={`Luis Alejandro Martínez Faneyth`}
       />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
     </Head>
   )
 }
