98275acbbf6091d0ea6e09fb16058d4b524bcf7d
Luis Alejandro Martínez Faneyth <luis@luisalejandro.org>
2023-07-18 21:45:48 -0400

    Updating portfolio

--- next.config.js
+++ next.config.js
@@ -1,32 +1,46 @@
 module.exports = {
   images: {
-    domains: ['imgix.cosmicjs.com'],
+    domains: ["imgix.cosmicjs.com"],
   },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: [{
-        loader:'@svgr/webpack',
-        options: {
-          "svgoConfig": {
-            plugins: [{
-              name: 'preset-default',
-              params: {
-                overrides: {
-                  prefixIds: false,
-                  cleanupIDs: false,
-                  removeNonInheritableGroupAttrs: false,
-                  moveElemsAttrsToGroup: false,
-                  moveGroupAttrsToElems: false,
-                  collapseGroups: false,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: "prefixIds",
+                  params: {
+                    prefixIds: false,
+                  },
                 },
-              },
-            }],
+                {
+                  name: "cleanupIds",
+                  params: {
+                    cleanupIds: false,
+                  },
+                },
+                {
+                  name: "preset-default",
+                  params: {
+                    overrides: {
+                      removeNonInheritableGroupAttrs: false,
+                      moveElemsAttrsToGroup: false,
+                      moveGroupAttrsToElems: false,
+                      collapseGroups: false,
+                    },
+                  },
+                },
+              ],
+            },
           },
-        }
-      }]
+        },
+      ],
     });
 
     return config;
-  }
-}
+  },
+};
